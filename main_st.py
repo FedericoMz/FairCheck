a0350119a444f83592c0ac541678f203af06f420
Federico Mazzoni <80719913+FedericoMz@users.noreply.github.com>
2024-02-02 02:57:50 +0100

    Update main_st.py

--- main_st.py
+++ main_st.py
@@ -1,7 +1,7 @@
 import streamlit as st
-from trust_main import *
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 
 def get_discrimination (df, sensitive_attributes, class_name):
 
