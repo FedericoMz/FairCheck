617cda23f3b1906ad1905c15e3f85f9163bcd160
Federico Mazzoni <80719913+FedericoMz@users.noreply.github.com>
2024-02-02 03:18:50 +0100

    Update main_st.py

--- main_st.py
+++ main_st.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
+import plotly.graph_objects as go
 
 def get_discrimination (df, sensitive_attributes, class_name):
 
